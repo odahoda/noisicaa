54acdd251dd9226289f1f7ed986add2d5e7cc47e
Ben Niemann <pink@odahoda.de>
2015-12-03 00:32:40 +0100

    (Clumpsy) tests for context menu actions.

--- noisicaa/ui/sheet_view_test.py
+++ noisicaa/ui/sheet_view_test.py
@@ -4,6 +4,7 @@ import unittest
 from unittest import mock
 
 from PyQt5.QtGui import QCloseEvent
+from PyQt5.QtWidgets import QMenu
 
 from noisicaa import music
 from . import uitest_utils
@@ -20,7 +21,7 @@ class SheetViewTest(uitest_utils.UITest):
         self.project = EditorProject(self.app)
         self.project.sheets.clear()
         self.project.dispatch_command = mock.MagicMock()
-        self.project.dispatch_command.side_effect = AssertionError(
+        self.project.dispatch_command.side_effect = self.failureException(
             "Unexpected command")
         self.sheet = music.Sheet(name='test')
         self.project.sheets.append(self.sheet)
@@ -36,6 +37,66 @@ class SheetViewTest(uitest_utils.UITest):
         with view.inhibitUpdates():
             cmd.run(self.sheet)
 
+
+class MeasureItemTest(uitest_utils.UITest):
+    def setUp(self):
+        super().setUp()
+        self.window = None
+        self.project = EditorProject(self.app)
+        self.project.sheets.clear()
+        self.project.dispatch_command = mock.MagicMock()
+        self.project.dispatch_command.side_effect = self.failureException(
+            "Unexpected command")
+        self.sheet = music.Sheet(name='test')
+        self.project.sheets.append(self.sheet)
+
+    def test_menu_insert_measure(self):
+        self.project.dispatch_command.side_effect = None
+
+        view = sheet_view.SheetView(None, self.app, self.window, self.sheet)
+        track = view._tracks[0]
+        measure = track.measures[0]
+
+        # Not the best way to find and trigger a context menu action...
+        menu = QMenu()
+        measure.buildContextMenu(menu)
+        for action in menu.actions():
+            if action.text() == "Insert measure":
+                action.trigger()
+                break
+        else:
+            self.fail("No 'Insert measure' menu item found.")
+
+        self.assertEqual(self.project.dispatch_command.call_count, 1)
+        (target, cmd), _ = self.project.dispatch_command.call_args
+        self.assertEqual(target, '/sheet:test')
+        self.assertIsInstance(cmd, music.InsertMeasure)
+        self.assertEqual(cmd.tracks, [0])
+        self.assertEqual(cmd.pos, 0)
+
+    def test_menu_remove_measure(self):
+        self.project.dispatch_command.side_effect = None
+
+        view = sheet_view.SheetView(None, self.app, self.window, self.sheet)
+        track = view._tracks[0]
+        measure = track.measures[0]
+
+        # Not the best way to find and trigger a context menu action...
+        menu = QMenu()
+        measure.buildContextMenu(menu)
+        for action in menu.actions():
+            if action.text() == "Remove measure":
+                action.trigger()
+                break
+        else:
+            self.fail("No 'Remove measure' menu item found.")
+
+        self.assertEqual(self.project.dispatch_command.call_count, 1)
+        (target, cmd), _ = self.project.dispatch_command.call_args
+        self.assertEqual(target, '/sheet:test')
+        self.assertIsInstance(cmd, music.RemoveMeasure)
+        self.assertEqual(cmd.tracks, [0])
+        self.assertEqual(cmd.pos, 0)
 
 if __name__ == '__main__':
     unittest.main()
