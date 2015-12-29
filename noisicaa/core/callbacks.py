12d50849bd7e38e764e08dae4c63661f6229481e
Ben Niemann <pink@odahoda.de>
2015-12-29 01:57:31 +0100

    Add callbacks module (before I'm going to reimplement this again).

---
+++ noisicaa/core/callbacks.py
@@ -0,0 +1,82 @@
+#!/usr/bin/python3
+
+import logging
+import uuid
+import threading
+
+logger = logging.getLogger(__name__)
+
+class Listener(object):
+    """Only internally used by CallbackRegistry."""
+
+    def __init__(self, target, callback):
+        self.id = str(uuid.uuid4())
+        self.target = target
+        self.callback = callback
+
+
+class CallbackRegistry(object):
+    """A registry for callbacks.
+
+    Clients can register callbacks for certain targets that they are interested
+    in.
+
+    Targets are identified by arbitrary identifiers, which can be any object,
+    which is suitable as a dictionary key. How targets look like is defined
+    by the owner of the registry.
+
+    The arguments with which the callbacks are called is also defined by the
+    owner of the registry.
+    """
+
+    def __init__(self):
+        self._listeners = {}
+        self._target_map = {}
+        self._lock = threading.Lock()
+
+    def add_listener(self, target, callback):
+        """Register a new callback.
+
+        Args:
+          target: The target identifier for which you want to receive callbacks.
+          callback: A callable with will be called.
+
+        Returns:
+          A listener ID, which should be used to unregister this callback.
+        """
+
+        listener = Listener(target, callback)
+        with self._lock:
+            self._listeners[listener.id] = listener
+            self._target_map.setdefault(target, []).append(listener.id)
+        return listener.id
+
+    def remove_listener(self, listener_id):
+        """Remove a callback.
+
+        Args:
+          listener_id: The ID as returned by add_listener().
+
+        Raises:
+          KeyError: if the listener_id is not a valid callback.
+        """
+
+        with self._lock:
+            listener = self._listeners[listener_id]
+            del self._listeners[listener.id]
+            self._target_map[listener.target].remove(listener.id)
+
+    def callback(self, target, *args, **kwargs):
+        """Call all callbacks registered for a given target.
+
+        This method should only be called by the owner of the registry.
+
+        Args:
+          target: The target identifier for which the callbacks should be called.
+          args, kwargs: The arguments passed to all callbacks.
+        """
+
+        with self._lock:
+            for listener_id in self._target_map.get(target, []):
+                listener = self._listeners[listener_id]
+                listener.callback(*args, **kwargs)
---
+++ noisicaa/core/callbacks_test.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+
+import unittest
+from unittest import mock
+
+from . import callbacks
+
+class CallbackRegistryTest(unittest.TestCase):
+    def test_callbacks(self):
+        listener1 = mock.Mock()
+        listener2 = mock.Mock()
+
+        registry = callbacks.CallbackRegistry()
+        lid1 = registry.add_listener('foo', listener1)
+        lid2 = registry.add_listener('bar', listener2)
+
+        # Nothing gets called.
+        registry.callback('gnurz', 'arg0')
+        self.assertEqual(listener1.call_args_list, [])
+        self.assertEqual(listener2.call_args_list, [])
+
+        # Only listener1 gets called.
+        registry.callback('foo', 'arg1')
+        self.assertEqual(listener1.call_args_list, [mock.call('arg1')])
+        self.assertEqual(listener2.call_args_list, [])
+
+        # listener1 is not called again.
+        registry.remove_listener(lid1)
+        registry.callback('foo', 'arg2')
+        self.assertEqual(listener1.call_args_list, [mock.call('arg1')])
+
+        # Now listener2 gets called.
+        registry.callback('bar', 'arg3')
+        self.assertEqual(listener2.call_args_list, [mock.call('arg3')])
+
+        # listener2 is not called again.
+        registry.remove_listener(lid2)
+        registry.callback('bar', 'arg4')
+        self.assertEqual(listener2.call_args_list, [mock.call('arg3')])
+
+
+if __name__ == '__main__':
+    unittest.main()
