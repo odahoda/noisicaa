2751c02a4e56f31df2af194692ff8f31a339329e
Ben Niemann <pink@odahoda.de>
2016-09-10 00:22:15 +0200

    Basics of moving control points.

--- noisicaa/ui/control_track_item.py
+++ noisicaa/ui/control_track_item.py
@@ -17,6 +17,142 @@ from . import layout
 logger = logging.getLogger(__name__)
 
 
+class Handle(QtWidgets.QGraphicsRectItem):
+    def __init__(self, parent=None, graph=None, idx=None):
+        super().__init__(parent=parent)
+
+        self._graph = graph
+        self._index = idx
+
+        self.setRect(-3, -3, 6, 6)
+        self.setPen(Qt.black)
+        self.setBrush(QtGui.QBrush(Qt.NoBrush))
+
+        self._moving = False
+        self._move_handle_pos = None
+        self._moved = False
+
+    def mousePressEvent(self, evt):
+        if evt.button() == Qt.LeftButton:
+            self.grabMouse()
+            self._moving = True
+            self._move_handle_pos = evt.scenePos() - self.pos()
+            self._moved = False
+            evt.accept()
+            return
+
+        super().mousePressEvent(evt)
+
+    def mouseMoveEvent(self, evt):
+        if self._moving:
+            new_pos = evt.scenePos() - self._move_handle_pos
+
+            if new_pos.x() < 0:
+                new_pos.setX(0)
+            elif new_pos.x() > self._graph.width:
+                new_pos.setX(self._graph.width)
+
+            if new_pos.y() < 0:
+                new_pos.setY(0)
+            elif new_pos.y() > self._graph.height:
+                new_pos.setY(self._graph.height)
+
+            self.setPos(new_pos)
+
+            if self._index < len(self._graph.handles) - 1:
+                segment = self._graph.segments[self._index]
+                segment.setLine(QtCore.QLineF(new_pos, segment.line().p2()))
+
+            if self._index > 0:
+                segment = self._graph.segments[self._index - 1]
+                segment.setLine(QtCore.QLineF(segment.line().p1(), new_pos))
+
+            self._moved = True
+            evt.accept()
+            return
+
+        super().mouseMoveEvent(evt)
+
+    def mouseReleaseEvent(self, evt):
+        if evt.button() == Qt.LeftButton and self._moving:
+            if self._moved:
+                pass
+
+            self.ungrabMouse()
+            self._moving = False
+            evt.accept()
+            return
+
+        super().mouseReleaseEvent(evt)
+
+
+class ControlGraph(QGraphicsGroup):
+    def __init__(self, parent=None, track=None, size=None, widths=None):
+        super().__init__(parent=parent)
+
+        self._track = track
+        self._size = size
+        self._widths = widths
+
+        x = 0
+        for width in self._widths:
+            x += width
+
+            l = QtWidgets.QGraphicsLineItem(self)
+            l.setLine(x, 0, x, self.height)
+            l.setPen(QtGui.QColor(240, 240, 240))
+
+        frame = QtWidgets.QGraphicsRectItem(self)
+        frame.setRect(0, 0, self.width, self.height)
+        frame.setPen(QtGui.QColor(200, 200, 200))
+        frame.setBrush(QtGui.QBrush(Qt.NoBrush))
+
+        self.handles = []
+        self.segments = []
+
+        prev_x = 0
+        prev_timepos = music.Duration(0, 4)
+        prev_value = 0.0
+
+        def _value_to_y(v):
+            return int(self.height - int(self.height * v))
+
+        for point in self._track.points:
+            if point.timepos != prev_timepos:
+                x = prev_x + 50
+
+                if not self.handles:
+                    handle = Handle(parent=self, graph=self, idx=len(self.handles))
+                    handle.setPos(prev_x, _value_to_y(prev_value))
+                    self.handles.append(handle)
+
+                left_point = self.handles[-1].pos()
+                right_point = QtCore.QPointF(x, _value_to_y(point.value))
+
+                handle = Handle(parent=self, graph=self, idx=len(self.handles))
+                handle.setPos(right_point)
+                self.handles.append(handle)
+
+                segment = QtWidgets.QGraphicsLineItem(self)
+                segment.setLine(
+                    prev_x, _value_to_y(prev_value),
+                    x, _value_to_y(point.value))
+                segment.setPen(Qt.black)
+                self.segments.append(segment)
+
+                prev_x = x
+                prev_timepos = point.timepos
+                prev_value = point.value
+
+    @property
+    def width(self):
+        return self._size.width()
+
+    @property
+    def height(self):
+        return self._size.height()
+
+
 class ControlTrackLayout(layout.TrackLayout):
     def __init__(self, track):
         super().__init__()
@@ -42,7 +178,7 @@ class ControlTrackLayout(layout.TrackLayout):
 
     @property
     def height(self):
-        return 40
+        return 120
 
 
 class ControlTrackItemImpl(base_track_item.TrackItemImpl):
@@ -55,42 +191,16 @@ class ControlTrackItemImpl(base_track_item.TrackItemImpl):
     def renderTrack(self, y, track_layout):
         layer = self._sheet_view.layers[base_track_item.Layer.MAIN]
 
-        x = 0
-        for width in track_layout.widths[:-1]:
-            x += width
+        text = QtWidgets.QGraphicsSimpleTextItem(layer)
+        text.setText("> %s" % self._track.name)
+        text.setPos(0, y)
 
-            l = QtWidgets.QGraphicsLineItem(layer)
-            l.setLine(0, 0, 0, track_layout.height)
-            l.setPen(QtGui.QColor(200, 200, 200))
-            l.setPos(x, y)
-
-        r = QtWidgets.QGraphicsRectItem(layer)
-        r.setRect(0, 0, x, track_layout.height)
-        r.setPen(Qt.black)
-        r.setBrush(QtGui.QBrush(Qt.NoBrush))
-        r.setPos(0, y)
-
-        prev_x = 0
-        prev_timepos = music.Duration(0, 4)
-        prev_value = 0.0
-
-        def _value_to_y(v):
-            return int(track_layout.height * (1.0 - v))
-
-        for point in self._track.points:
-            if point.timepos != prev_timepos:
-                x = prev_x + 50
-
-                l = QtWidgets.QGraphicsLineItem(layer)
-                l.setLine(
-                    prev_x, _value_to_y(prev_value),
-                    x, _value_to_y(point.value))
-                l.setPen(Qt.black)
-                l.setPos(0, y)
-
-                prev_x = x
-                prev_timepos = point.timepos
-                prev_value = point.value
+        self._graph = ControlGraph(
+            parent=layer,
+            track=self._track,
+            size=QtCore.QSize(sum(track_layout.widths[:-1]), track_layout.height - 20),
+            widths=track_layout.widths[:-1])
+        self._graph.setPos(0, y + 20)
 
 
 class ControlTrackItem(ui_base.ProjectMixin, ControlTrackItemImpl):
