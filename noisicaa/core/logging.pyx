c1db71847fd54e0bbe1b422a7a2f1f93fc2355b6
Ben Niemann <pink@odahoda.de>
2017-09-24 11:12:19 +0200

    Reenable sending C++ logs to Python's logging system.
    With a workaround for https://github.com/cython/cython/issues/1877

--- noisicaa/core/logging.pyx
+++ noisicaa/core/logging.pyx
@@ -1,23 +1,36 @@
+from cpython.ref cimport PyObject
+from cpython.exc cimport PyErr_Fetch, PyErr_Restore
+from libc.stdio cimport fprintf, stderr
 import logging
 
 cdef void pylogging_cb(
     void* handle, const char* c_logger_name, LogLevel c_level, const char* c_msg) with gil:
-    logger_name = bytes(c_logger_name).decode('utf-8')
-    level = {
-        LogLevel.DEBUG: logging.DEBUG,
-        LogLevel.INFO: logging.INFO,
-        LogLevel.WARNING: logging.WARNING,
-        LogLevel.ERROR: logging.ERROR,
-        }[c_level]
-    msg = bytes(c_msg).decode('utf-8')
+    # Have to stash away any active exception, because otherwise exception handling
+    # might get confused.
+    # See https://github.com/cython/cython/issues/1877
+    cdef PyObject* exc_type
+    cdef PyObject* exc_value
+    cdef PyObject* exc_trackback
+    PyErr_Fetch(&exc_type, &exc_value, &exc_trackback)
 
-    logger = logging.getLogger(logger_name)
-    logger.log(level, msg)
+    try:
+        logger_name = bytes(c_logger_name).decode('utf-8')
+        level = {
+            LogLevel.DEBUG: logging.DEBUG,
+            LogLevel.INFO: logging.INFO,
+            LogLevel.WARNING: logging.WARNING,
+            LogLevel.ERROR: logging.ERROR,
+            }[c_level]
+        msg = bytes(c_msg).decode('utf-8')
+
+        logger = logging.getLogger(logger_name)
+        logger.log(level, msg)
+    except:
+        fprintf(stderr, "Failed to log message %s\n", c_msg);
+
+    PyErr_Restore(exc_type, exc_value, exc_trackback)
 
 
 def init_pylogging():
-    # Not activating the PyLogSink, because it screws up exception handling.
-    # See https://github.com/cython/cython/issues/1877
-    # cdef LogSink* sink = new PyLogSink(NULL, pylogging_cb)
-    # LoggerRegistry.get_registry().set_sink(sink)
-    pass
+    cdef LogSink* sink = new PyLogSink(NULL, pylogging_cb)
+    LoggerRegistry.get_registry().set_sink(sink)
