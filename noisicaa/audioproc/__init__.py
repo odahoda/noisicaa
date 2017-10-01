f016ecd1791bf7a27cad26bee96da9130d2506ee
Ben Niemann <pink@odahoda.de>
2017-10-01 17:43:19 +0200

    Delete more dead code.

--- noisicaa/audioproc/__init__.py
+++ noisicaa/audioproc/__init__.py
@@ -1,6 +1,4 @@
 from .audioproc_client import AudioProcClientMixin
-from .data import FrameContext
-from .pipeline_mutations_capnp import PipelineMutation
 from .mutations import (
     AddNode,
     RemoveNode,
--- noisicaa/audioproc/pipeline_mutations.capnp
+++
@@ -1,5 +0,0 @@
-@0xde8ec529c8db8814;
-
-struct PipelineMutation {
-  pickled @0 :Data;
-}
