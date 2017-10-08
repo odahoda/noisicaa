e2ea776078f857e7c7341512fde4e3d65a1f11d9
Ben Niemann <pink@odahoda.de>
2017-10-08 16:35:25 +0200

    Preprocess SVG symbols at build time, not runtime.

--- noisicaa/ui/svg_symbol.py
+++ noisicaa/ui/svg_symbol.py
@@ -23,13 +23,9 @@
 import io
 import logging
 import os.path
-import re
-import subprocess
 import sys
-import tempfile
 from xml.etree import ElementTree
 
-import cssutils
 from PyQt5 import QtCore
 from PyQt5.QtSvg import QSvgRenderer, QGraphicsSvgItem
 from PyQt5.QtWidgets import QGraphicsItemGroup
@@ -60,75 +56,10 @@ def _fqattr(ns, attr):
 class SvgSymbol(object):
     _cache = {}
 
-    def __init__(self, path, cache_dir=None):
-        self._orig_path = path
-        if not os.path.isfile(self._orig_path):
-            raise FileNotFoundError(self._orig_path)
-
-        if cache_dir is None: # pragma: no cover
-            cache_dir = os.path.dirname(self._orig_path)
-
-        root, ext = os.path.splitext(os.path.basename(self._orig_path))
-        self._path = os.path.join(
-            cache_dir,
-            root + '.stripped' + ext)
-
-    def strip_dom(self, tree):
-        root = tree.getroot()
-
-        for grp in root.findall('svg:g', namespaces):
-            if grp.get(_fqattr('inkscape', 'groupmode')) != 'layer':
-                continue
-
-            style = cssutils.parseStyle(grp.get('style', ''))
-            if style.getProperty('display').value == 'none':
-                root.remove(grp)
-                continue
-
-        elem = root.find('sodipodi:namedview', namespaces)
-        if elem is not None:  # pragma: no branch
-            root.remove(elem)
-
-        elem = root.find('svg:metadata', namespaces)
-        if elem is not None:  # pragma: no branch
-            root.remove(elem)
-
-        with tempfile.TemporaryDirectory() as tmpdirname:
-            svgpath = os.path.join(tmpdirname, 'svg')
-            with open(svgpath, 'wb') as fp:
-                tree.write(fp, encoding='utf-8', xml_declaration=True)
-
-            # OMG.. there must be a better way...
-            # OTOH, distributed archives should always contain the stripped
-            # version, so this would only ever run on dev machines.
-            pngpath = os.path.join(tmpdirname, 'png')
-            subprocess.check_call(['inkscape', svgpath, '-e', pngpath],
-                                  stdout=subprocess.DEVNULL,
-                                  stderr=subprocess.DEVNULL)
-
-            info = subprocess.check_output(
-                ['convert', pngpath, '-trim', 'info:-'])
-            info = info.decode('ascii')
-            m = re.match(
-                r'.* PNG (\d+)x(\d+) (\d+)x(\d+)\+(\d+)\+(\d+) .*$', info)
-            assert m is not None, info
-            width, height, canvas_width, canvas_height, xoffset, yoffset = [
-                int(g) for g in m.groups()]
-            xoffset -= 8
-            yoffset -= 8
-            width += 16
-            height += 16
-
-            root.set(
-                'viewBox', '%d %d %d %d' % (xoffset, yoffset, width, height))
-            root.set(
-                _fqattr('noisicaa', 'origin-x'),
-                '%f' % ((canvas_width / 2 - xoffset) / 2))
-            root.set(
-                _fqattr('noisicaa', 'origin-y'),
-                '%f' % ((canvas_height / 2 - yoffset) / 2))
-            root.set('width', '%f' % (width / 2))
-            root.set('height', '%f' % (height / 2))
+    def __init__(self, path):
+        self._path = path
+        if not os.path.isfile(self._path):
+            raise FileNotFoundError(self._path)
 
     @utils.memoize
     def get_dom(self):
@@ -136,24 +67,8 @@ class SvgSymbol(object):
 
     @utils.memoize
     def get_xml(self):
-        xml = None
-        if (os.path.exists(self._path)
-                and os.path.getmtime(self._orig_path) <= os.path.getmtime(self._path)):
-            logger.debug("Found stripped symbol %s.", self._path)
-            with open(self._path, 'rb') as fp:
-                xml = fp.read()
-        else:
-            logger.debug("Stripping symbol %s...", self._orig_path)
-            tree = ElementTree.parse(self._orig_path)
-            self.strip_dom(tree)
-            buf = io.BytesIO()
-            tree.write(buf, encoding='utf-8', xml_declaration=True)
-            xml = buf.getvalue()
-
-            with open(self._path, 'wb') as fp:
-                fp.write(xml)
-
-        return xml
+        with open(self._path, 'rb') as fp:
+            return fp.read()
 
     def get_origin(self):
         tree = self.get_dom()
@@ -194,7 +109,7 @@ def paintSymbol(painter, symbol_name, pos):
 class SymbolItem(QGraphicsItemGroup):
     _cache = {}
 
-    def __init__(self, symbol, parent=None, cache_dir=None):
+    def __init__(self, symbol, parent=None):
         super().__init__(parent)
 
         self._symbol = symbol
@@ -203,7 +118,7 @@ class SymbolItem(QGraphicsItemGroup):
             renderer, svg_symbol = self._cache[self._symbol]
         except KeyError:
             path = os.path.join(DATA_DIR, 'symbols', '%s.svg' % self._symbol)
-            svg_symbol = SvgSymbol(path, cache_dir=cache_dir)
+            svg_symbol = SvgSymbol(path)
             renderer = QSvgRenderer(svg_symbol.get_xml())
             self._cache[self._symbol] = (renderer, svg_symbol)
 
--- noisicaa/ui/svg_symbol_test.py
+++ noisicaa/ui/svg_symbol_test.py
@@ -22,9 +22,6 @@
 
 import os.path
 import unittest
-import tempfile
-import shutil
-from unittest import mock
 from xml.etree.ElementTree import ElementTree
 
 from . import svg_symbol
@@ -33,56 +30,33 @@ TESTDATA_DIR = os.path.join(os.path.dirname(__file__), 'testdata')
 
 
 class SvgSymbolTest(unittest.TestCase):
-    def setUp(self):
-        self.cache_dir = tempfile.mkdtemp()
-        self.addCleanup(shutil.rmtree, self.cache_dir)
-
     def test_orig_not_found(self):
         with self.assertRaises(FileNotFoundError):
             svg_symbol.SvgSymbol('/does-not-exist')
 
     def test_get_dom(self):
         sym = svg_symbol.SvgSymbol(
-            os.path.join(TESTDATA_DIR, 'symbol.svg'),
-            cache_dir=self.cache_dir)
+            os.path.join(TESTDATA_DIR, 'symbol.svg'))
         dom = sym.get_dom()
         self.assertIsInstance(dom, ElementTree)
 
     def test_get_xml(self):
         sym = svg_symbol.SvgSymbol(
-            os.path.join(TESTDATA_DIR, 'symbol.svg'),
-            cache_dir=self.cache_dir)
+            os.path.join(TESTDATA_DIR, 'symbol.svg'))
         xml1 = sym.get_xml()
         self.assertIsInstance(xml1, bytes)
-        self.assertTrue(
-            os.path.isfile(os.path.join(self.cache_dir, 'symbol.stripped.svg')))
-
-        # No load it again. This time we should load it from the cached path.
-        sym = svg_symbol.SvgSymbol(
-            os.path.join(TESTDATA_DIR, 'symbol.svg'),
-            cache_dir=self.cache_dir)
-        sym.strip_dom = mock.MagicMock()
-        xml2 = sym.get_xml()
-        self.assertFalse(sym.strip_dom.called)
-
-        self.assertEqual(xml1, xml2)
 
     def test_get_origin(self):
         sym = svg_symbol.SvgSymbol(
-            os.path.join(TESTDATA_DIR, 'symbol.svg'),
-            cache_dir=self.cache_dir)
+            os.path.join(TESTDATA_DIR, 'symbol.svg'))
         ox, oy = sym.get_origin()
         self.assertIsInstance(ox, float)
         self.assertIsInstance(oy, float)
 
 
 class SymbolItemTest(unittest.TestCase):
-    def setUp(self):
-        self.cache_dir = tempfile.mkdtemp()
-        self.addCleanup(shutil.rmtree, self.cache_dir)
-
     def test_init(self):
-        item = svg_symbol.SymbolItem('rest-quarter', cache_dir=self.cache_dir)
+        item = svg_symbol.SymbolItem('rest-quarter')
         bbox = item.body.boundingRect()
         self.assertGreater(bbox.width(), 0)
         self.assertGreater(bbox.height(), 0)
