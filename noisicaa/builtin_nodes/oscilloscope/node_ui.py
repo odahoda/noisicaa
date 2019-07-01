2743cac468544d164c32ea5360e0e7471ea3b86e
Ben Niemann <pink@odahoda.de>
2019-07-01 02:04:24 +0200

    Oscilloscope: Fancy layout.

--- noisicaa/builtin_nodes/oscilloscope/node_ui.py
+++ noisicaa/builtin_nodes/oscilloscope/node_ui.py
@@ -33,6 +33,9 @@ from PyQt5 import QtWidgets
 from noisicaa import core
 from noisicaa import music
 from noisicaa.ui import slots
+from noisicaa.ui import dynamic_layout
+from noisicaa.ui import int_dial
+from noisicaa.ui import control_value_dial
 from noisicaa.ui import ui_base
 from noisicaa.ui.graph import base_node
 from . import model
@@ -132,6 +135,32 @@ class Oscilloscope(slots.SlotContainer, QtWidgets.QWidget):
         hold_time = self.holdTime()
         return [1, 2, 5][hold_time % 3] * 10.0 ** (hold_time // 3)
 
+    @classmethod
+    def formatTimeScale(cls, time_scale: int) -> str:
+        mul = [1, 2, 5][time_scale % 3]
+        time_scale //= 3
+        if time_scale <= -4:
+            return '%dµs' % (mul * 10 ** (time_scale + 6))
+        elif time_scale <= -1:
+            return '%dms' % (mul * 10 ** (time_scale + 3))
+        else:
+            return '%ds' % (mul * 10 ** time_scale)
+
+    @classmethod
+    def formatHoldTime(cls, hold_time: int) -> str:
+        mul = [1, 2, 5][hold_time % 3]
+        hold_time //= 3
+        if hold_time <= -4:
+            return '%dµs' % (mul * 10 ** (hold_time + 6))
+        elif hold_time <= -1:
+            return '%dms' % (mul * 10 ** (hold_time + 3))
+        else:
+            return '%ds' % (mul * 10 ** hold_time)
+
+    @classmethod
+    def formatYScale(cls, y_scale: int) -> str:
+        return '%g' % ([1, 2, 5][y_scale % 3] * 10.0 ** (y_scale // 3))
+
     def addValues(self, values: Iterable[float]) -> None:
         if self.__plot_rect is None:
             return
@@ -188,6 +217,9 @@ class Oscilloscope(slots.SlotContainer, QtWidgets.QWidget):
     def sizeHint(self) -> QtCore.QSize:
         return QtCore.QSize(100, 100)
 
+    def minimumSizeHint(self) -> QtCore.QSize:
+        return QtCore.QSize(60, 60)
+
     def resizeEvent(self, evt: QtGui.QResizeEvent) -> None:
         if evt.size().width() > 20 and evt.size().height() > 20:
             self.__show_major_grid = True
@@ -227,10 +259,12 @@ class Oscilloscope(slots.SlotContainer, QtWidgets.QWidget):
         if self.__show_x_labels:
             border_bottom += x_label_height
 
-        if evt.size().width() >= border_left + border_right + 10 and evt.size().height() >= border_top + border_bottom + 10:
+        if (evt.size().width() >= border_left + border_right + 10
+                and evt.size().height() >= border_top + border_bottom + 10):
             self.__plot_rect = QtCore.QRect(
                 border_left, border_right,
-                evt.size().width() - border_left - border_right, evt.size().height() - border_top - border_bottom)
+                evt.size().width() - border_left - border_right,
+                evt.size().height() - border_top - border_bottom)
 
             self.__time_step_size = self.__plot_rect.height() // 2
 
@@ -272,7 +306,9 @@ class Oscilloscope(slots.SlotContainer, QtWidgets.QWidget):
                 x = 0
                 while x < w:
                     for g in (1, 2, 3, 4):
-                        painter.fillRect(x + int(g * self.__time_step_size / 5), 0, 1, h, self.__grid_color)
+                        painter.fillRect(
+                            x + int(g * self.__time_step_size / 5), 0, 1, h,
+                            self.__grid_color)
                     x += self.__time_step_size
 
             if self.__show_major_grid:
@@ -294,16 +330,7 @@ class Oscilloscope(slots.SlotContainer, QtWidgets.QWidget):
             painter.setPen(self.__label_color)
 
             if self.__show_x_labels and self.__time_step_size <= w:
-                time_scale = self.timeScale()
-                mul = [1, 2, 5][time_scale % 3]
-                time_scale //= 3
-                if time_scale <= -4:
-                    t1 = '%dµs' % (mul * 10 ** (time_scale + 6))
-                elif time_scale <= -1:
-                    t1 = '%dms' % (mul * 10 ** (time_scale + 3))
-                else:
-                    t1 = '%ds' % (mul * 10 ** time_scale)
-
+                t1 = self.formatTimeScale(self.timeScale())
                 t1r = self.__label_font_metrics.boundingRect(t1)
                 painter.drawText(
                     min(self.__plot_rect.left() + self.__time_step_size - t1r.width() // 2,
@@ -312,8 +339,7 @@ class Oscilloscope(slots.SlotContainer, QtWidgets.QWidget):
                     t1)
 
             if self.__show_y_labels:
-                y1 = '%g' % self.absYScale()
-
+                y1 = self.formatYScale(self.yScale())
                 y1r = self.__label_font_metrics.boundingRect(y1)
                 painter.drawText(
                     self.__plot_rect.left() - y1r.width() - 2,
@@ -366,6 +392,9 @@ class OscilloscopeNodeWidget(ui_base.ProjectMixin, core.AutoCleanupMixin, QtWidg
         self.__listeners['node-messages'] = self.audioproc_client.node_messages.add(
             '%016x' % self.__node.id, self.__nodeMessage)
 
+        label_font = QtGui.QFont(self.font())
+        label_font.setPointSizeF(0.8 * label_font.pointSizeF())
+
         self.__plot = Oscilloscope()
 
         self.__paused = QtWidgets.QPushButton()
@@ -379,51 +408,86 @@ class OscilloscopeNodeWidget(ui_base.ProjectMixin, core.AutoCleanupMixin, QtWidg
         self.__step.clicked.connect(self.__plot.step)
         self.__paused.toggled.connect(self.__step.setEnabled)
 
-        self.__time_scale = QtWidgets.QSpinBox()
+        self.__time_scale = int_dial.IntDial()
+        self.__time_scale.setMinimumSize(56, 56)
         self.__time_scale.setRange(-12, 3)
+        self.__time_scale.setDefault(-5)
+        self.__time_scale.setDisplayFunc(Oscilloscope.formatTimeScale)
         self.__time_scale.valueChanged.connect(self.__plot.setTimeScale)
         self.__time_scale.setValue(-5)
 
-        self.__hold_time = QtWidgets.QSpinBox()
+        self.__time_scale_label = QtWidgets.QLabel("Time", self)
+        self.__time_scale_label.setFont(label_font)
+        self.__time_scale_label.setAlignment(Qt.AlignHCenter)
+
+        self.__hold_time = int_dial.IntDial()
+        self.__hold_time.setMinimumSize(56, 56)
         self.__hold_time.setRange(-6, 3)
+        self.__hold_time.setDefault(-3)
+        self.__hold_time.setDisplayFunc(Oscilloscope.formatHoldTime)
         self.__hold_time.valueChanged.connect(self.__plot.setHoldTime)
         self.__hold_time.setValue(-3)
 
-        self.__y_scale = QtWidgets.QSpinBox()
+        self.__hold_time_label = QtWidgets.QLabel("Hold", self)
+        self.__hold_time_label.setFont(label_font)
+        self.__hold_time_label.setAlignment(Qt.AlignHCenter)
+
+        self.__y_scale = int_dial.IntDial()
+        self.__y_scale.setMinimumSize(56, 56)
         self.__y_scale.setRange(-18, 18)
+        self.__y_scale.setDefault(1)
+        self.__y_scale.setDisplayFunc(Oscilloscope.formatYScale)
         self.__y_scale.valueChanged.connect(self.__plot.setYScale)
         self.__y_scale.setValue(1)
 
-        self.__y_offset = QtWidgets.QDoubleSpinBox()
+        self.__y_scale_label = QtWidgets.QLabel("Y Zoom", self)
+        self.__y_scale_label.setFont(label_font)
+        self.__y_scale_label.setAlignment(Qt.AlignHCenter)
+
+        self.__y_offset = control_value_dial.ControlValueDial()
+        self.__y_offset.setMinimumSize(56, 56)
         self.__y_offset.setRange(-1.0, 1.0)
-        self.__y_offset.setDecimals(2)
-        self.__y_offset.setSingleStep(0.05)
+        self.__y_offset.setDefault(0.0)
         self.__y_offset.valueChanged.connect(self.__plot.setYOffset)
         self.__y_offset.setValue(0.0)
 
-        l3 = QtWidgets.QHBoxLayout()
-        l3.setContentsMargins(0, 0, 0, 0)
-        l3.addWidget(self.__paused)
-        l3.addWidget(self.__step)
-        l3.addStretch(1)
+        self.__y_offset_label = QtWidgets.QLabel("Y Off", self)
+        self.__y_offset_label.setFont(label_font)
+        self.__y_offset_label.setAlignment(Qt.AlignHCenter)
 
-        l2 = QtWidgets.QFormLayout()
-        l2.setContentsMargins(0, 0, 0, 0)
-        l2.addRow('Time scale:', self.__time_scale)
-        l2.addRow('Hold time:', self.__hold_time)
-        l2.addRow('Y scale:', self.__y_scale)
-        l2.addRow('Y offset:', self.__y_offset)
-
-        l4 = QtWidgets.QVBoxLayout()
-        l4.setContentsMargins(0, 0, 0, 0)
-        l4.addLayout(l3)
-        l4.addLayout(l2)
-
-        l1 = QtWidgets.QHBoxLayout()
-        l1.setContentsMargins(0, 0, 0, 0)
-        l1.addLayout(l4)
-        l1.addWidget(self.__plot, 1)
-        self.setLayout(l1)
+        layout = dynamic_layout.DynamicLayout(
+            dynamic_layout.VBox(
+                dynamic_layout.Widget(self.__plot, stretch=1),
+                dynamic_layout.HBox(
+                    dynamic_layout.VBox(
+                        dynamic_layout.Widget(self.__paused, priority=5),
+                        dynamic_layout.Widget(self.__step, priority=5),
+                    ),
+                    dynamic_layout.VBox(
+                        dynamic_layout.Widget(self.__time_scale, priority=3),
+                        dynamic_layout.Widget(self.__time_scale_label, priority=4),
+                        stretch=1,
+                    ),
+                    dynamic_layout.VBox(
+                        dynamic_layout.Widget(self.__hold_time, priority=3),
+                        dynamic_layout.Widget(self.__hold_time_label, priority=4),
+                        stretch=1,
+                    ),
+                    dynamic_layout.VBox(
+                        dynamic_layout.Widget(self.__y_scale, priority=3),
+                        dynamic_layout.Widget(self.__y_scale_label, priority=4),
+                        stretch=1,
+                    ),
+                    dynamic_layout.VBox(
+                        dynamic_layout.Widget(self.__y_offset, priority=3),
+                        dynamic_layout.Widget(self.__y_offset_label, priority=4),
+                        stretch=1,
+                    ),
+                ),
+                spacing=2,
+            )
+        )
+        self.setLayout(layout)
 
 
     def __nodeMessage(self, msg: Dict[str, Any]) -> None:
